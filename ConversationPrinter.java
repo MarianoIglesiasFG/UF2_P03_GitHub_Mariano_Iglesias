4d4acd6423c1374a46b70259fb838770b9b76519
MarianoIglesiasFG <133921949+MarianoIglesiasFG@users.noreply.github.com>
2023-05-18 00:10:50 +0200

    Add files via upload

---
+++ ConversationPrinter.java
@@ -0,0 +1,23 @@
+package conversaciones;
+
+public class ConversationPrinter {
+    public static void main(String[] args) {
+        System.out.println("Person A: Hello!");
+        System.out.println("Person B: Hi there!");
+        System.out.println("Person A: How are you doing?");
+        System.out.println("Person B: I'm doing well, thank you. How about you?");
+        System.out.println("Person A: I'm good too. Just working on some programming exercises.");
+        System.out.println("Person B: That's great. Programming can be fun.");
+        System.out.println("Person A: Yes, it definitely is. What programming languages do you know?");
+        System.out.println("Person B: I know Java, Python, and a bit of C++.");
+        System.out.println("Person A: That's impressive. I'm still learning Java.");
+        System.out.println("Person B: Don't worry, with practice, you'll become proficient.");
+        System.out.println("Person A: I hope so. By the way, do you have any favorite programming books?");
+        System.out.println("Person B: Yes, I really like 'Clean Code' by Robert C. Martin.");
+        System.out.println("Person A: I've heard about that one. I'll definitely check it out. Thanks!");
+        System.out.println("Person B: You're welcome! If you have any more questions, feel free to ask.");
+        System.out.println("Person A: Sure, I'll keep that in mind. Have a great day!");
+        System.out.println("Person B: You too! Take care and happy coding!");
+        System.out.println("Person A: Bye!");
+    }
+}
